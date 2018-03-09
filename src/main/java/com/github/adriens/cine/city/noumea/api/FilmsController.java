deb7c332faee8a3fabd783c91eb2246f51e91bc8
adriens <adrien.sales@gmail.com>
2018-03-10 07:02:39 +1100

    Version upgrade

--- src/main/java/com/github/adriens/cine/city/noumea/api/FilmsController.java
+++ src/main/java/com/github/adriens/cine/city/noumea/api/FilmsController.java
@@ -24,39 +24,45 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class FilmsController {
-    
+
     final static Logger logger = LoggerFactory.getLogger(FilmsController.class);
+
+    public FilmsController(){
+        java.util.logging.Logger.getLogger("com.gargoylesoftware.htmlunit").setLevel(java.util.logging.Level.OFF);
+        java.util.logging.Logger.getLogger("org.apache.http").setLevel(java.util.logging.Level.OFF);        
     
+    }
     @RequestMapping(
-            value={"/", "/today"})
+            value = {"/", "/today"})
     public ArrayList<Film> today() throws IOException {
         FilmsWrapper wrapper = new FilmsWrapper();
         return wrapper.getFilmsDuJour();
     }
-    
+
     @RequestMapping("/bests")
     public ArrayList<Film> bests() throws IOException {
         FilmsWrapper wrapper = new FilmsWrapper();
         return wrapper.getTop20();
     }
+
     @RequestMapping("/worsts")
     public ArrayList<Film> worsts() throws IOException {
         FilmsWrapper wrapper = new FilmsWrapper();
         return wrapper.getWorsts20();
     }
-    
+
     @RequestMapping("/contact")
     public CinemaContact contact() throws IOException {
         CinemaContact contact = new CinemaContact();
         return contact;
     }
-    
+
     @RequestMapping(value = "/film/{filmid}")
     public FilmDetails filmDetails(
-            @PathVariable("filmid")  int filmid) throws IOException {
+            @PathVariable("filmid") int filmid) throws IOException {
         //CinemaContact contact = new CinemaContact();
         logger.info("Got movie id : <" + filmid + ">");
         FilmsWrapper wrap = new FilmsWrapper();
         return wrap.getDetailsOfFilm(filmid);
-    }    
+    }
 }
